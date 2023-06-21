6da0e0272586dbc935cd5b7920869b40cae55c3b
TarasMysiura <tarasmysiura@gmail.com>
2023-06-21 15:20:16 +0300

    Create task-2.py

---
+++ task-2.py
@@ -0,0 +1,14 @@
+v = float(input("введіть значення щвидкості авто "))
+# v = 293
+
+
+t = 3.32
+
+s = round(t * v, 1) 
+# print(type(s))
+
+print("відстань s = t * v = ", t,  " * " , v, " = ", s , "км")
+print(s)
+# print(int(s))
+# print(v1)
+# print(type(v1))
