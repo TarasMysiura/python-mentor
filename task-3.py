965c082612589721b902c3e3bb0deb20b09a7bd6
TarasMysiura <tarasmysiura@gmail.com>
2023-07-01 18:06:47 +0300

    +

--- task-3.py
+++ task-3.py
@@ -5,13 +5,15 @@ action = input('введіть дію (+ - * /): ')
 b = float(input('введіть друге число: '))
 
 
-if (action == "+"):
+if (action == "+" or action == " +" or action == "+ " or action == " + "):
     print("Сумма чисел дорівнює ", str(a+b))
-elif (action == "-"):
+elif (action == "-" or action == " -" or action == "- " or action == " - "):
     print("Різниця чисел дорівнює ", str(a-b))
-elif (action == "/"):
+elif (action == "/" or action == " /" or action == "/ " or action == " / "):
     print("Частка чисел дорівнює", str(a/b))
-elif (action == "*"):
+elif (action == "*" or action == " *" or action == "* " or action == " * "):
     print('Добуток чисел дорівнює', str(a*b))
+elif (action == "^" or action == " ^" or action == "^ " or action == " ^ "):
+    print('Піднесення числа ', a,' до степеня ', b, ' дорівнює', str(a**b))
 else:
     print('невірно набрано')
